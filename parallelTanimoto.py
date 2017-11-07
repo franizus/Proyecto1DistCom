883a1435d8395caab5d505e4903ebf1703b73772
Unknown <franizus@live.com>
2017-11-07 17:47:40 -0500

    added open_file and analyze_string functions

---
+++ parallelTanimoto.py
@@ -0,0 +1,23 @@
+import csv
+
+def open_file(chemicals):
+    with open("chemicals.tsv") as fd:
+        rd = csv.reader(fd, delimiter="\t", quotechar='"')
+        for row in rd:
+            chemicals.update({row[0]:row[2]})
+
+def analyze_string(smiles, letters):
+    for letter in smiles:
+        if letter in letters.keys():
+            new_value = letters.get(letter) + 1
+            letters.update({letter:new_value})
+        else:
+            letters.update({letter:1})
+    letters.update({'@':1})
+    
+chemicals = {}
+letters = {}
+open_file(chemicals)
+analyze_string(chemicals.get('ZINC00006923'), letters)
+print(chemicals)
+print(letters)
