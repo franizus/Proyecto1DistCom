de71b4f3a04859b5f3e418575ed49aa04db357f4
Francisco Izurieta <franizus@live.com>
2017-11-08 09:44:25 -0500

    added number_of_elements and jac_tan_coef methods

--- parallelTanimoto.py
+++ parallelTanimoto.py
@@ -8,19 +8,49 @@ def open_file(chemicals):
 
 def analyze_string(smiles, letters):
     for letter in smiles:
-        if letter in letters.keys():
+        if letter in letters:
             new_value = letters.get(letter) + 1
             letters.update({letter:new_value})
         else:
             letters.update({letter:1})
-    letters.update({'@':1})
+    if '@' in letters:
+        letters.update({'@':1})
 
 def common_elements(elem_a, elem_b):
-    
-    
+    copy_b = elem_b.copy()
+    nc = 0
+    for key_a in elem_a:
+        for key_b in copy_b:
+            if key_a == key_b:
+                nc += min(elem_a.get(key_a), elem_b.get(key_b))
+                del copy_b[key_b]
+                break
+    return nc
+
+def number_of_elements(elem):
+    number = 0
+    for key in elem:
+        number += elem.get(key)
+    return number
+
+def jac_tan_coefficient(na, nb, nc):
+    return round((nc / (na + nb - nc)), 2)
+
 chemicals = {}
-letters = {}
+letters_a = {}
+letters_b = {}
 open_file(chemicals)
-analyze_string(chemicals.get('ZINC00006923'), letters)
+analyze_string(chemicals.get('ZINC00006923'), letters_a)
+analyze_string(chemicals.get('ZINC04843014'), letters_b)
 print(chemicals.get('ZINC00006923'))
-print(letters)
+print(letters_a)
+print(chemicals.get('ZINC04843014'))
+print(letters_b)
+na = number_of_elements(letters_a)
+nb = number_of_elements(letters_b)
+nc = common_elements(letters_a, letters_b)
+coe = jac_tan_coefficient(na, nb, nc)
+print(na)
+print(nb)
+print(nc)
+print(coe)
