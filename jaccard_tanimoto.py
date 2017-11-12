bd3d05fdd223fc1eba4dcaad456478f8995927b3
Unknown <franizus@live.com>
2017-11-12 15:39:52 -0500

    changed to printo to console to save memory

--- jaccard_tanimoto.py
+++ jaccard_tanimoto.py
@@ -1,9 +1,7 @@
 """ Parallel Jaccard/Tanimoto coefficient module """
 import csv
 import math
-import sys
 import time
-import tempfile
 import multiprocessing
 from threading import Thread
 
@@ -11,7 +9,7 @@ from threading import Thread
 def open_file():
     """Opens a tsv file and returns a list with the data."""
     chemicals_list = []
-    with open("chemicals.tsv") as file:
+    with open("ZINC_chemicals.tsv") as file:
         reader = csv.reader(file, delimiter="\t", quotechar='"')
         for row in reader:
             row_aux = (row[1], row[3])
@@ -66,21 +64,16 @@ def jac_tan_coefficient(elements_a, elements_b, common_elements):
 def get_pivots(chemicals_length, number_processors):
     """Calculates the pivots to divide the chemicals between the threads."""
     pivots_list = []
-    pivots_list.append(1)
+    tolal_elements = (chemicals_length**2) / 2
+    pivots_list.append(0)
     for i in range(1, number_processors):
         pivots_list.append(
-            int(round(math.sqrt(i / number_processors) * chemicals_length)))
+            int(round(tolal_elements - (math.sqrt(i / number_processors) * chemicals_length))))
     pivots_list.append(chemicals_length - 1)
-    copy_pivots = pivots_list.copy()
-    n_pivots = len(pivots_list)
-    for i in range(1, n_pivots - 1):
-        pivots_list[i] = copy_pivots[n_pivots - i] - \
-            copy_pivots[n_pivots - i - 1] + pivots_list[i - 1]
-    pivots_list[0] = 0
     return pivots_list
 
 
-def fill_compared_list(chemicals_list, pivot_min, pivot_max, compared_temp_file):
+def fill_compared_list(chemicals_list, pivot_min, pivot_max, compared_chemicals_list):
     """Fills a list with the comparison of the chemical compounds between two pivots in the list."""
     for i in range(pivot_min, pivot_max):
         for j in range(i + 1, len(chemicals_list)):
@@ -90,8 +83,7 @@ def fill_compared_list(chemicals_list, pivot_min, pivot_max, compared_temp_file)
                                        number_chemical_elements(letters_b),
                                        number_common_elements(letters_a, letters_b))
             row = (chemicals_list[i][0], chemicals_list[j][0], coef)
-            compared_temp_file.write(
-                row[0] + "\t" + row[1] + "\t" + str(row[2]) + "\n")
+            compared_chemicals_list.append(row)
 
 
 def start_join_all(thread_array):
@@ -102,17 +94,11 @@ def start_join_all(thread_array):
         thread.join()
 
 
-def write_file(compared_chemicals_list):
-    """Writes a tsv file with the compared chemicals."""
-    with open("chem_sim_total.tsv", "w") as record_file:
-        record_file.write("Chem_ID_1\tChem_ID_2\tTanimoto_similarity\n")
-        for temp_file in compared_chemicals_list:
-            try:
-                print(temp_file.name)
-                temp_file.seek(0)
-                record_file.write(temp_file.read())
-            finally:
-                temp_file.close()
+def print_to_console(number_threads, compared_chemicals_list):
+    """Prints in console the compared chemicals."""
+    for i in range(number_threads):
+        for j in compared_chemicals_list[i]:
+            print(j)
 
 
 if __name__ == "__main__":
@@ -123,7 +109,7 @@ if __name__ == "__main__":
     START_TIME = time.time()
     compared_chemicals = []
     for index in range(NUMBER_THREADS):
-        compared_chemicals.append(tempfile.NamedTemporaryFile(mode='r+'))
+        compared_chemicals.append([])
     threads = []
     for index in range(NUMBER_THREADS):
         threads.append(Thread(target=fill_compared_list, args=(
@@ -133,5 +119,5 @@ if __name__ == "__main__":
     start_join_all(threads)
 
     END_TIME = time.time()
+    print_to_console(NUMBER_THREADS, compared_chemicals)
     print(END_TIME - START_TIME)
-    write_file(compared_chemicals)
