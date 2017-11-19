8fb8b2e055ead242b06b91ee8dc254e55a78e0c1
Unknown <franizus@live.com>
2017-11-19 14:04:14 -0500

    removed files

--- jaccard_tanimoto.c
+++ jaccard_tanimoto.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <omp.h>
 
-#define MAX_ELEMENTS 16
+#define MAX_ELEMENTS 15000
 
 typedef struct Chemical
 {
-    char id[12];
-    char compound[150];
+    char* id;
+    char* compound;
 } chemical;
 
 //Opens a tsv file and returns a list with the data.
 void openFile(chemical *chemicalsList)
 {
     FILE *fp;
-    char buff[255], temp[10];
+    char buff[255], temp[50];
     fp = fopen("chemicals.tsv", "r");
     int i = 0;
     while(fgets(buff, sizeof(buff), fp) != NULL)
     {
         sscanf(buff, "%s\t%s\t%s\t%s", temp, chemicalsList[i].id, temp, chemicalsList[i].compound);
-        printf("%s\n\n", chemicalsList[i].id);
-        printf("%s\n\n", chemicalsList[i].compound);
+        printf("%s\n", chemicalsList[i].id);
+        printf("%s\n", chemicalsList[i].compound);
         i++;
     }
     fclose(fp);
@@ -28,6 +31,6 @@ void openFile(chemical *chemicalsList)
 
 int main()
 {
-    chemical chemicalsList[MAX_ELEMENTS];
+    chemical *chemicalsList = (chemical*)malloc(sizeof(chemical) * 12422);
     openFile(chemicalsList);
 }
